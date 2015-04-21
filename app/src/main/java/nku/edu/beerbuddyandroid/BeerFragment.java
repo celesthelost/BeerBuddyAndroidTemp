193581c7be126ed46cce13ff3284a80eecef8182
unknown <jdreyer.uso@gmail.com>
2015-04-21 19:15:52 -0400

    temporary beerbuddyandroid repository

---
+++ app/src/main/java/nku/edu/beerbuddyandroid/BeerFragment.java
@@ -0,0 +1,94 @@
+package nku.edu.beerbuddyandroid;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+
+public class BeerFragment extends Fragment
+{
+
+
+    private ListView beerlist;
+    @Override
+    public void onAttach( Activity activity )
+    {
+        super.onAttach( activity ) ;
+
+    }
+
+    @Override
+    public void onCreate( Bundle state )
+    {
+        super.onCreate( state ) ;
+
+    }
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle state )
+    {
+        System.out.println( "onCreateView fired" ) ;
+
+
+        View v = inflater.inflate( R.layout.beer_fragment, container ) ;
+
+
+        return v ;
+    }
+    public void onActivityCreated( Bundle state )
+    {
+        super.onActivityCreated( state ) ;
+    }
+
+    @Override
+    public void onStart( )
+    {
+        super.onStart( ) ;
+    }
+
+    @Override
+    public void onResume( )
+    {
+        super.onResume( ) ;
+    }
+
+    @Override
+    public void onPause( )
+    {
+        super.onPause( ) ;
+    }
+    @Override
+    public void onSaveInstanceState( Bundle state )
+    {
+        super.onSaveInstanceState( state ) ;
+    }
+    @Override
+    public void onStop( )
+    {
+        super.onStop( ) ;
+    }
+
+    @Override
+    public void onDestroyView( )
+    {
+        super.onDestroyView( ) ;
+    }
+
+    @Override
+    public void onDestroy( )
+    {
+        super.onDestroy( ) ;
+    }
+
+    @Override
+    public void onDetach( )
+    {
+        super.onDetach( ) ;
+    }
+
+
+}
